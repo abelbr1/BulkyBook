82d216c150fc4069c3d8835190c4d8f257e07690
abelbr1 <abelbrhanuab@gmail.com>
2023-05-11 15:17:45 -0700

    Adding progress

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,10 +1,13 @@
-﻿namespace BulkyBookWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models;
+
+public class Category
 {
-    public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int DisplayOrder { get; set; }
-        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
-    }
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+    public DateTime CreatedDateTime { get; set; } = DateTime.Now;
 }
--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -1,7 +1,15 @@
+
+using BulkyBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(Options => Options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
